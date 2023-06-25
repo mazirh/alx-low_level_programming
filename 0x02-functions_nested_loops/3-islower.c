6d10b8eae9c221aea2c7c4d0ff810035d3e34521
mazirh <oumayma.mazirh.73@gmail.com>
2023-06-25 13:17:36 -0700

    Returning 1 if the character is lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,13 +5,12 @@
  *@c: character to test
  *Return: 1 (True) else 0 (False)
  */
-
 int _islower(int c)
 {
-if (c <= 'a' && c <= 'Z')
+if (c <= 'a' && c <= 'z')
 {
 return (1);
 }
 return (0);
 }
-      
+
