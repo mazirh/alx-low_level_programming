c41820094f8bc280c821c17affc895d566c2dac9
mazirh <oumayma.mazirh.73@gmail.com>
2023-06-25 08:50:28 -0700

    Printing _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,18 +2,18 @@
 
 /**
  * main - Entry point
+ * Description: printing _putchar
  * Return:0
  */
 
 int main(void)
 {
-char c[9] = "_putchar";
-int i = 0;
-    while (i < 8)
-         {
-          _putchar(c[i]);
-          i++;
-          }
-    _putchar('\n');
-    return (0);
+   char c[] = "_putchar\n";
+   int i = 0;
+   while (c[i] !='\0')
+             {
+               _putchar(c[i]);
+               i++;
+             }
+   return (0);
 }
