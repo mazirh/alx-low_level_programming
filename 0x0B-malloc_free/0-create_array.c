22da6cc73fc0e26c3e89c972bd6e97151683511e
mazirh <oumayma.mazirh.73@gmail.com>
2023-07-17 13:31:58 -0700

    Creating an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,7 +7,7 @@
  *@c: char to assign
  *Description: create an array of size size and assign char c
  *Return a pointer if size 0 or  NULL
- *
+ *Returns NULL if size = 0
  **/
 char *create_array(unsigned int size, char c)
 {
