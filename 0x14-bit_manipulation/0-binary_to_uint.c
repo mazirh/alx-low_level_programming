7ab46f0aee7d30e442eead2e9d6bfad539ec6d04
mazirh <oumayma.mazirh.73@gmail.com>
2023-10-29 07:43:47 -0700

    Converting to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,11 +13,10 @@ unsigned int binary_to_uint(const char *b)
 	if (!b)
 		return (0);
 
-	for (i = 0; b[i]; i++)
-	{
+	for (i = 0; b[i]; i++) {
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		        dec_val = 2 * dec_val + (b[i] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
 	return (dec_val);
